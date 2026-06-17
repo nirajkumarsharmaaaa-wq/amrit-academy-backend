68091785aefaafb410f058a86fea0bbad61d7ea3
Niraj Sharma <nirajkumarsharmaaaa@gmail.com>
2026-06-17 17:06:58 +0530

    Updated baseURL to point to Render API

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://amrit-academy-frontend.vercel.app/' })); // Vercel ka URL yahan daalein
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected Successfully'))
